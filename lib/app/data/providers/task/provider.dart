b329645ee835683d920a83c33d73a61227cee757
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-05 11:45:07 +0300

    Task Provider & Json Decode-Encode Implemented

---
+++ lib/app/data/providers/task/provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:task_app_getx/app/core/utils/keys.dart';
+import 'package:task_app_getx/app/data/services/storage/services.dart';
+
+import '../../models/task.dart';
+
+class TaskProvider{
+  StorageService _stroage = Get.find<StorageService>();
+  
+  List<Task> readTasks(){
+    var tasks=<Task>[];
+    jsonDecode(_stroage.read(taskKey).toString()).forEach((e)=>tasks.add(Task.fromJson(e)));
+    return tasks;
+  }
+  void writeTas(List<Task> tasks){
+    _stroage.write(taskKey, jsonEncode(tasks));
+  }
+}
