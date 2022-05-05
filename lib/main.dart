46049ca00a0ed188def470331e8267b889faae56
Eren Karaboğa <darkbull1073@gmail.com>
2022-05-05 10:54:11 +0300

    Json Serialization & Equatable Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:task_app/app/modules/home/view.dart';
+
 import 'app/data/services/storage/services.dart';
+import 'app/modules/home/view.dart';
 Future<void> main() async {
   await GetStorage.init();
   await Get.putAsync(() => StorageService().init());
